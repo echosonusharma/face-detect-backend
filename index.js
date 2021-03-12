a06ba03e194216434627da61e5fd945137df79e6
Sonu Sharma <echosonusharma@gmail.com>
2021-03-13 00:46:02 +0530

    heroku psql db setup

--- index.js
+++ index.js
@@ -16,10 +16,8 @@ const app = express();
 const pdb = knex({
     client: 'pg',
     connection: {
-        host: '127.0.0.1',
-        user: process.env.DB_USER,
-        password: process.env.DB_PASSWORD,
-        database: process.env.DB_NAME
+        connectionString: process.env.DATABASE_URL,
+        ssl: true
     }
 });
 
